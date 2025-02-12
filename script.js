ee9792bc23ef8ab36f4e2e9ef954ce6d2dfb00a8
Bruno Gomes <106351872+viego777@users.noreply.github.com>
2025-02-11 22:54:35 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const mensagem = "Olá! Fiquei interessado em seus serviços!";
-const mensagemCodificada = encodeURIComponent(mensagem);
-const link = `https://www.instagram.com/direct/?text=${mensagemCodificada}`;
-
-console.log(link);
-// Saída: https://www.instagram.com/direct/?text=Ol%C3%A1%2C%20como%20posso%20ajudar%3F
